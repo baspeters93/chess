ad39b0d4789c4b48f1ddff2db39b74691a227402
Bas Peters <baspeters93@gmail.com>
2014-08-25 21:32:43 +0200

    Implemented all pieces, other minor changes

--- piece.py
+++ piece.py
@@ -16,22 +16,19 @@ class Piece(object):
 
         self.setPossibleMoves()
 
-        #set name of piece in compliance to FEN
+        # set name of piece in compliance to FEN
         pieceName = self.__class__.__name__
         if self.color == "white":
             self.name = pieceName[:1]
         else:
             self.name = pieceName.lower()[:1]
 
+    @property
     def __str__(self):
 
         return self.name
 
     def move(self, to):
-
-        pass
-
-    def setPossibleMoves(self):
 
         pass
 
@@ -45,7 +42,9 @@ class Pawn(Piece):
         therefore it is necessary to make a distinction based on the piece color
         """
 
-        #TODO: Fix ugly checks and ifs
+        # TODO: Fix ugly checks and ifs
+        self.possibleMoves = []
+
         row, col = self.position[0], self.position[1]
         if self.color == "white":
 
@@ -61,7 +60,6 @@ class Pawn(Piece):
 
             if self.board.exists(upright) and not self.board.isEmpty(upright):
                 if self.board.isEnemy(upright, self.color):
-                    #WE CAN MURDER IT
                     self.possibleMoves.append(upright)
 
             if self.board.exists(upleft) and not self.board.isEmpty(upleft):
@@ -95,24 +93,89 @@ class Rook(Piece):
         Rooks can advance in any vertical or horizontal direction, they can castle with the king as
         long as they havent made a single move yet.
         """
+        # TODO: castling
+        self.possibleMoves = []
+
+        self.possibleMoves.extend(self.board.paths(self, "up", "down", "left", "right"))
 
 
 class Knight(Piece):
-    def test(self):
-        pass
+    def setPossibleMoves(self):
+        """
+        Knights move two squares horizontally or vertically and then move one square left or right.
+        """
+        self.possibleMoves = []
+        row, col = self.position[0], self.position[1]
+
+        # TODO: find something less ugly
+        tiles = [(row + 2, col), (row - 2, col), \
+                 (row, chr(ord(col) + 2)), (row, chr(ord(col) - 2))]
+
+        for tile in tiles:
+
+            if tile[1] == col:
+                left = self.board.getAdjacent(tile, "left")
+                right = self.board.getAdjacent(tile, "right")
+
+                if self.board.exists(left):
+                    if self.board.isEmpty(left) or self.board.isEnemy(left, self.color):
+                        self.possibleMoves.append(left)
+
+                if self.board.exists(right):
+                    if self.board.isEmpty(right) or self.board.isEnemy(right, self.color):
+                        self.possibleMoves.append(right)
+
+            else:
+                up = self.board.getAdjacent(tile, "up")
+                down = self.board.getAdjacent(tile, "down")
+
+                if self.board.exists(up):
+                    if self.board.isEmpty(up) or self.board.isEnemy(up, self.color):
+                        self.possibleMoves.append(up)
+
+                if self.board.exists(down):
+                    if self.board.isEmpty(down) or self.board.isEnemy(down, self.color):
+                        self.possibleMoves.append(down)
 
 
 class Bishop(Piece):
-    def test(self):
-        pass
+    def setPossibleMoves(self):
+        """
+        Bishops can move only in queer directions
+        """
+        self.possibleMoves = []
+
+        self.possibleMoves.extend(self.board.paths(self, "upright", "upleft", "downright", "downleft"))
 
 
 class King(Piece):
     def setPossibleMoves(self):
-        pass
+        """
+        Kings can move to any square that is directly adjacent to them.
+        Kings can also castle.
+        """
+        self.possibleMoves = []
+
+        for direction in self.board.directions:
+            tile = self.board.getAdjacent(self.position, direction)
+
+            if self.board.exists(tile):
+                if self.board.isEmpty(tile):
+                    self.possibleMoves.append(tile)
+
+                else:
+                    if self.board.isEnemy(tile, self.color):
+                        self.possibleMoves.append(tile)
+
+    # TODO: implement castling
 
 
 class Queen(Piece):
-    def test(self):
-        pass
+    def setPossibleMoves(self):
+        """
+        Queens can move in all directions.
+        """
+        self.possibleMoves = []
 
+        self.possibleMoves.extend(self.board.paths(self, "up", "down", "left", "right", \
+                                                   "downright", "downleft", "upright", "upleft"))
